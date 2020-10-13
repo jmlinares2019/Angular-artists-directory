0b826e1693b6dedf80b68ea4e9e1da0d5543ad45
jmlinares2019 <jlinaresmar@gmail.com>
2020-10-13 18:59:37 +0200

    Sets 2-way data binding in input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+// NgModule permite, entre otras cosas, el 2-way binding [()]
 import { NgModule } from '@angular/core';
+// FormsModule también es necesario para la directiva ngModel
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
