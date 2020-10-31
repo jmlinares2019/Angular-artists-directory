3c77a098b7f0432702e7bc9843850d60f5444c1f
jmlinares2019 <jlinaresmar@gmail.com>
2020-10-31 19:15:41 +0100

    Generates pipe to search by names and applies it to app component

---
+++ src/app/search-artists.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchArtists'
+})
+export class SearchArtistsPipe implements PipeTransform {
+
+  transform(pipeData, pipeModifier): any {
+    return pipeData.filter(eachItem =>{
+      return (
+        eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase())
+        // los elementos del array (eachItem) cuyo 'name' (en minúsculas) 
+        // que incluyan el término de búsqueda (pipeModifier, también en minúsculas) 
+        )
+    });
+  }
+
+}
