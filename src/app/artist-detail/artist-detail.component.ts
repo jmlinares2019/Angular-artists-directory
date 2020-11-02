3de26587e75219c4e985f281b0be71deb8dcc936
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-02 21:16:28 +0100

    Changes var name from 'artist' to 'currentArtist' to differentiate artist-item component var 'artist' from artist-detail var 'currentArtist'

--- src/app/artist-detail/artist-detail.component.ts
+++ src/app/artist-detail/artist-detail.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ArtistDetailComponent implements OnInit {
 
-  @Input() artist: any;
+  @Input() currentArtist: any;
 
   constructor() { }
 
