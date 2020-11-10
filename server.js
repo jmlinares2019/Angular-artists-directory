fb64c60cc09393af3abefac92140e60fc0729e62
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-10 14:53:33 +0100

    cambio ruta en express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/learnAngular'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Server listening');
